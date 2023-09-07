dd630fc4027d2023d7f9d38abd50e7228803815e
Alethepata <alessia.patane@gmail.com>
2023-09-07 15:39:01 +0200

    [Init] Script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+1. Salvare in 2 variabili il prompt dell'inserimento dei km e dell'età.
+2. Moltiplicare i km per 0,21. 
+3. Creare 2 IF: -sconto minorenni(-20%) -sconto over 65(-40%).
+4. Creare la stringa di output.
+5. Effettuare l'innerHTML dell'output nel tag designato.
+*/ 
