d0cb712c0d36cd13df9a3fca163dc63893978b74
Alethepata <alessia.patane@gmail.com>
2023-09-07 16:21:41 +0200

    [Add] IF

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
 1. Salvare in 2 variabili il prompt dell'inserimento dei km e dell'età.
 2. Moltiplicare i km per 0,21. 
-3. Creare 2 IF: -sconto minorenni(-20%) -sconto over 65(-40%).
+3. Creare 3 IF: -sconto minorenni(-20%) -sconto over 65(-40%) -prezzo base.
 4. Creare la stringa di output.
 5. Effettuare l'innerHTML dell'output nel tag designato.
 */ 
@@ -14,6 +14,17 @@ const costoKm = km * 0.21;
 const scontoMinorenni = (costoKm * 20) / 100;
 const scontoOver = (costoKm * 40) / 100;
 let message
+
+if(age < 18){
+    const prezzoFinale = costoKm - scontoMinorenni;
+    message= prezzoFinale
+}else if(age > 65){
+    const prezzoFinale = costoKm - scontoOver;
+    message= prezzoFinale
+} else{
+    const prezzoFinale =costoKm;
+    message= prezzoFinale;
+}
 
 
 
